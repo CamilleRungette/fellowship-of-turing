e0154279c40c762ab94791d0b5d07e62c0e2b727
CamilleRungette <c.rungette@gmail.com>
2020-02-13 17:09:30 +0100

    routes get ok + views

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,34 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/work', function(req, res, next){
+
+  res.render('work')
+})
+
+router.get('/projects', function(req, res, next){
+
+  res.render('projects')
+})
+
+router.get('/team', function(req, res, next){
+
+  res.render('team')
+})
+
+router.get('/articles', function(req, res, next){
+
+  res.render('articles')
+})
+
+router.get('/contact', function(req, res, next){
+
+  res.render('contact')
+})
+
+
+
+
+
+
 module.exports = router;
